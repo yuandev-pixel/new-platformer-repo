f1ab093c2fb7631bf25f9ad084ac6660e3cd7dc7
[yuan dev] <[niannian2024@yeah.net]>
2025-07-04 19:22:34 +0800

    try faild

--- main.py
+++ main.py
@@ -26,11 +26,12 @@ screen = pygame.display.set_mode(SCREEN_SIZE, flags=test_tag, vsync = 1)
 clock = pygame.time.Clock()
 camera_x = 0
 camera_y = 0
-cya = 0
 target_camera_x = 0
 target_camera_y = 0
 previous_camera_x = 1
 previous_camera_y = 1
+dx = 0
+dy = 0
 
 player_idle_frames = [
     pygame.transform.scale(
@@ -57,9 +58,9 @@ real_mouse_tile_pos=(0,0)
 preview_tile = []
 for i in range(-2,96):
     preview_tile.append(pygame.transform.scale(pygame.image.load("./assets/tiles/sprite_"+str(i)+".png"),(16,16)))
-the_tile = 2
+the_tile = -2
 
-with open("assets/map1.json") as json_file:
+with open("./assets/map1.json") as json_file:
     try:
         data = json.load(json_file)
         map_data = data["map"]
@@ -68,6 +69,8 @@ with open("assets/map1.json") as json_file:
         data = {"map":{},"other":{"cx":0,"cy":0}}
         map_data = data["map"]
         player_data = data["other"]
+with open("./assets/hit_box.json") as wow:
+    hitbox_assign = json.load(wow)
 
 pen = render.RenderPen(screen)
 tile_map = tiles.TileGrid(map_data)
@@ -104,35 +107,69 @@ while True:
     key = pygame.key.get_pressed()
     if key[pygame.K_a]:
         camera_x -= math.ceil(0.1 * delta)
+        dx = math.ceil(0.1 * delta)
     if key[pygame.K_d]:
         camera_x += math.ceil(0.1 * delta)
+        dx = -math.ceil(0.1 * delta)
     if key[pygame.K_s] and edit:
         camera_y += math.ceil(0.1 * delta)
+        dy = math.ceil(0.1 * delta)
     if key[pygame.K_w]:
         if edit:
             camera_y -= math.ceil(0.1 * delta)
+            dy = -math.ceil(0.1 * delta)
         else:
-            cya=-1
-        camera_y -= math.ceil(0.1 * delta)
+            dy=-1
+    if not edit:
+        camera_y+=dy
+        dy+=0.05
+    a_fake.shift(-camera_x, -camera_y)
+    tile_map.reload(map_data)
+    tile_state = tile_map.get_pos(camera_x, camera_y)
+    for object in tile_state:
+        print("hey")
+        try:
+            the_hitbox = hit_boxes[hitbox_assign[tile_map.start_pos[object]["type"]]["type"]].move(object["x"],object["y"])
+            if player_idle.hitbox.colliderect(the_hitbox):
+                camera_x -= dx
+                camera_y -= dy
+                a_fake.shift(-camera_x, -camera_y)
+                tile_map.reload(map_data)
+                tile_state = tile_map.get_pos(camera_x, camera_y)
+                print("p")
+                pygame.draw.rect(screen,"blue",(the_hitbox.topleft[0],the_hitbox.topleft[1],16,16))
+                break
+        except:
+           print("nope")
+           continue
+
+    #editor
     if edit and key[pygame.K_g]:
         camera_x=round(camera_x)
         camera_y=round(camera_y)
         real_mouse_tile_pos=(round(real_mouse_tile_pos[0]),round(real_mouse_tile_pos[1]))
         mouse_tile_pos=(round(mouse_tile_pos[0]),round(mouse_tile_pos[1])) # type: ignore
-
     if key[pygame.K_e]:
         try:
             the_tile = map_data[str(real_mouse_tile_pos[0]*75)+"."+str(real_mouse_tile_pos[1])]["type"]
         except:
-            print("la")
             the_tile = 0
     if (key[pygame.K_LSHIFT] or key[pygame.K_RSHIFT]) and key[pygame.K_s]:
-        with open(join("./assets/",input("file name:")),mode="w+") as f:
+        with open(join("./assets/",input("file name:")),mode="w") as f:
             data={"map":map_data,"other":{"cx":camera_x,"cy":camera_y}}
             json.dump(data, f, ensure_ascii=False, indent=4)
+    if (key[pygame.K_LSHIFT] or key[pygame.K_RSHIFT]) and key[pygame.K_l]:
+        with open(join("./assets/",input("file name:")),mode="r") as json_file:
+            try:
+                data = json.load(json_file)
+                map_data = data["map"]
+                player_data = data["other"]
+            except:
+                data = {"map":{},"other":{"cx":0,"cy":0}}
+                map_data = data["map"]
+                player_data = data["other"]
     if (key[pygame.K_LSHIFT] or key[pygame.K_RSHIFT]) and key[pygame.K_e]:
         edit = not edit
-
     if lt==5:
         if key[pygame.K_DOWN]:
             the_tile += 8
@@ -154,32 +191,10 @@ while True:
                 the_tile-=1
             the_tile += 1
             lt = 0
-        print(the_tile)
     else:
         if lt<5:
             lt+=1
     
-    if not edit:
-        camera_y+=cya
-        cya+=0.05
-        
-    print(edit)
-
-    #更新角色
-    player_idle.update(round(SCREEN_CENTER[0]) - 24, round(SCREEN_CENTER[1]) - 48)
-
-    #格子逻辑
-    tile_state = tile_map.get_pos(camera_x, camera_y)
-
-    #选择格子
-    mouse_pos = pygame.mouse.get_pos()
-    a_fake.shift(-camera_x, -camera_y)
-    mouse_tile_pos = mouse_pos[0]//16*16, mouse_pos[1]//16*16
-    real_mouse_tile_pos = mouse_tile_pos[0]/16 + camera_x, mouse_tile_pos[1]/16 + camera_y
-    print(real_mouse_tile_pos)
-    print(camera_x,camera_y)
-
-    #做关卡
     if  pygame.mouse.get_pressed()[0] and edit:
         map_data[str(real_mouse_tile_pos[0]*75)+"."+str(real_mouse_tile_pos[1])]={
         "type":str(the_tile),
@@ -187,6 +202,15 @@ while True:
         "y":real_mouse_tile_pos[1]-camera_y
         }
         tile_map.reload(map_data)
+        
+    #更新角色
+    player_idle.update(round(SCREEN_CENTER[0]) - 24, round(SCREEN_CENTER[1]) - 48)
+
+
+    #选择格子
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_tile_pos = mouse_pos[0]//16*16, mouse_pos[1]//16*16
+    real_mouse_tile_pos = mouse_tile_pos[0]/16 + camera_x, mouse_tile_pos[1]/16 + camera_y
 
     #绘制部分
 
